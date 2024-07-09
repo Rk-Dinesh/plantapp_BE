0acc2f5193a8aeb79ecef4cb085833c7fa667efb
Rk-Dinesh <rdkumar881@gmail.com>
2024-07-09 18:58:59 +0530

    index

--- index.js
+++ index.js
@@ -4,14 +4,28 @@ const app = express();
 const path = require('path')
 var cors = require('cors')
 const CustomerRouter = require('./Routes/CustomerRouter');
+
+const port = process.env.PORT || 3000;
+
+
+app.use(cors({
+    "Access-Control-Allow-Origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+}))
+
 app.use(express.json());
-app.use(cors())
-
-
 app.use(bodyParser.json());
 app.use(express.static('img'));
 app.use('/', CustomerRouter);
-const port = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to PLANTAPP");
+  });
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
